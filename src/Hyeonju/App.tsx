13715fda75234715b5dbdd7e1275cc61ce28f298
Hyeonju Kang <46iz.us2@gmail.com>
2023-01-11 18:56:15 +0900

    Add App component in the main.tsx

--- src/Hyeonju/App.tsx
+++ src/Hyeonju/App.tsx
@@ -1,5 +1,6 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import MatzipList from './components/MatzipList'
+import "bootstrap/dist/css/bootstrap.css"
 
 export type MatzipType = {
   no: number;
@@ -9,28 +10,28 @@ export type MatzipType = {
 }
 
 const spot = '강남';
-const addResult = (x:string, y:string) => {
+const addResult = (x: string, y: string) => {
   return (
     <div className='card card-body bg-light mb-3'>
-      {x+y}
+      {x + y}
     </div>
   )
 }
 
 const App = () => {
   const [list, setList] = useState<Array<MatzipType>>([
-    {no:1, name:'위트앤미트', stationExit: 11, revisitIntention: true},
-    {no:2, name:'왓쇼이켄', stationExit: 4, revisitIntention: false},
-    {no:3, name:'기리야마본진', stationExit: 4, revisitIntention: true},
-    {no:4, name:'청류벽', stationExit: 5, revisitIntention: false},
-    {no:5, name:'땀땀', stationExit: 11, revisitIntention: true} 
+    { no: 1, name: '위트앤미트', stationExit: 11, revisitIntention: true },
+    { no: 2, name: '왓쇼이켄', stationExit: 4, revisitIntention: false },
+    { no: 3, name: '기리야마본진', stationExit: 4, revisitIntention: true },
+    { no: 4, name: '청류벽', stationExit: 5, revisitIntention: false },
+    { no: 5, name: '땀땀', stationExit: 11, revisitIntention: true }
   ])
   return (
     <div className='container'>
       <h2>맛집 리스트 {spot}!</h2>
       <hr className='dash-style' />
       {addResult('만족스러운 저녁식사를 위한', ' 강남역 맛집 리스트')}
-      <MatzipList matzips={list}/>
+      <MatzipList matzips={list} />
     </div>
   )
 }
